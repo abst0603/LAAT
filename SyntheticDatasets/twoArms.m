eb58fd4e21eda55ae9b796c27b7061a729c6822a
abst0603 <abolfazl.taghribi@gmail.com>
2022-06-25 23:41:40 +0200

    Add the new version of two arms

--- SyntheticDatasets/twoArms.m
+++ SyntheticDatasets/twoArms.m
@@ -1,6 +1,6 @@
 clc
 clear all
-numofpoints = 3000;%how much points on the manifold
+numofpoints = 3000;%how many points on the manifold
 r = linspace(0.5,2.5,numofpoints);%medial axis of 2d manifold
 randvalue = 0.5 * r .* (rand(1,numofpoints)-0.5);% increase the width of manifold
 r = r + randvalue;
@@ -8,19 +8,12 @@ tetha = linspace(0,pi,numofpoints);% the curvature is from 0 to pi
 x = r.*cos(tetha);
 y = r.*sin(tetha);
 z = linspace(0,2.5,numofpoints) + rand(1,numofpoints)*0.2; % uniform noise in height too
-figure,plot3(x,y,z,'b.');
-axis equal
+dataset = [x',y',z'];
 
-noisepoints = rand(6000,3);
-noisepoints(:,1) = noisepoints(:,1) * 5 - 3.5;
-noisepoints(:,2) = noisepoints(:,2) * 4 - 1;
-noisepoints(:,3) = noisepoints(:,3) * 3.5 - 0.5;
-
-dataset = [[x',y',z'];noisepoints];
-figure,plot3(dataset(:,1),dataset(:,2),dataset(:,3),'b.');
-axis equal
-%dlmwrite('swissroll.csv',dataset,'precision',7);
-
+numofpoints = 1000;%how many points on the manifold
+r = linspace(0.5,2.5,numofpoints);%medial axis of 2d manifold
+randvalue = 0.5 * r .* (rand(1,numofpoints)-0.5);% increase the width of manifold
+r = r + randvalue;
 tetha2 = linspace(pi,2*pi,numofpoints);% the curvature is from pi to 0
 x2 = r.*cos(tetha2)-2;
 y2 = r.*sin(tetha2)+1.5;
@@ -28,3 +21,13 @@ z2 = linspace(0,2.5,numofpoints) + rand(1,numofpoints)*0.2; % uniform noise in h
 dataset = [dataset;[x2',y2',z2']];
 figure,plot3(dataset(:,1),dataset(:,2),dataset(:,3),'b.');
 axis equal
+
+noisepoints = rand(8000,3);
+noisepoints(:,1) = noisepoints(:,1) * 5 - 3.5;
+noisepoints(:,2) = noisepoints(:,2) * 4 - 1;
+noisepoints(:,3) = noisepoints(:,3) * 3.5 - 0.5;
+
+dataset = [dataset;noisepoints];
+figure,plot3(dataset(:,1),dataset(:,2),dataset(:,3),'b.');
+axis equal
+%dlmwrite('swissroll.csv',dataset,'precision',7);
